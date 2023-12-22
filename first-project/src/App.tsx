be9a1e532d34f16bae5dfbeefe9f16966b0c3459
Andre Bauzil <andregbauzil@gmail.com>
2023-12-22 11:54:34 -0300

    Adição de estilização em JS, usando classe porém melhor usar lib styled

--- first-project/src/App.tsx
+++ first-project/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Object } from './components/Object'
+import AppRoutes from './Routes'
 
 
 function App() {
@@ -15,11 +16,22 @@ function App() {
   
   return (
     <>
+      <AppRoutes />
       {objects.map((object, index) => {
         return <Object key={index} text={object} />
       })}
 
-      <button onClick={createObject}>Add Object</button>
+      <button 
+        onClick={createObject}
+        style={{
+          backgroundColor: '#8257e6',
+          border: 0,
+          padding: '6px 12px',
+          color: '#fff'
+        }}
+        >
+          Add Object
+      </button>
     </>
   )
 }
